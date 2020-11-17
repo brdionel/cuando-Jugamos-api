3a9efe2f5e848a155708dbe6bb5dd4cd1b486e60
Bruno Vicente <brunovicente32@gmail.com>
2020-11-17 17:20:09 -0300

    Fix/Delete Reminder Schema

--- src/reminder/dto/reminder.dto.ts
+++
@@ -1,4 +0,0 @@
-export class Reminder {
-    fecha: Date
-    text: string
-}
--- src/reminder/reminder.controller.ts
+++
@@ -1,8 +0,0 @@
-import { Controller, Put, Post, Get, Delete, Res, HttpStatus, Param } from '@nestjs/common';
-import { ReminderService } from './reminder.service'
-
-@Controller('reminder')
-export class ReminderController {
-    
-    constructor(private reminderService: ReminderService ){}
-}
